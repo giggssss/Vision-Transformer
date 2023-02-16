a0ac4cd4daf663d37d510b080ec0da83cd4a8545
Seokmin Yun <ysm861230@gmail.com>
2023-02-16 14:57:24 +0900

    Train & Eval code create. And refactored.

--- eval.py
+++ eval.py
@@ -1,43 +1,105 @@
 from __future__ import print_function
+from PIL import Image
+from torchvision import transforms
+from tqdm import tqdm
 
 import glob
 import os
 import torch
 import shutil
-import torch.nn as nn
-import torch.optim as optim
-from linformer import Linformer
-from PIL import Image
-from sklearn.model_selection import train_test_split
-from torch.optim.lr_scheduler import StepLR
-from torch.utils.data import DataLoader, Dataset
-from torchvision import datasets, transforms
-from tqdm import tqdm
+import random
+import requests
+from io import BytesIO
+import argparse
+
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 
-from vit_pytorch.efficient import ViT
+def parse_args():
+    parser = argparse.ArgumentParser(description='Day and Night Classifier')
+    parser.add_argument('--data_type', default='u', help='data type f(file), u(url) or d(dir)')
+    parser.add_argument('--data_path', default=r'D:\Data\train', help='image file path dir or url')
+    parser.add_argument('--checkpoint', default='save2_gpu.pt', help='checkpoint file')
+    parser.add_argument('--print', default=False, action='store_true', help='print on/off')
+    parser.add_argument('--file_copy', default=False, action='store_true', help='file_copy on/off')
 
-device = 'cpu'
-os.makedirs('data', exist_ok=True)
+    args = parser.parse_args()
+    return args
 
-train_dir = 'data/train'
-train_list = glob.glob(os.path.join(train_dir, '*.jpg'))
 
-model = torch.load('save.pt')
-model.eval()
+def file_list_load(path):
+    data_list = glob.glob(os.path.join(path, '*.jpg'))
+    # evl_list = glob.glob(os.path.join(path, "/**/*.jpg"), recursive=True)
+    random.shuffle(data_list)
 
-totensor = transforms.ToTensor()
-for file in train_list:
-    img = Image.open(file, 'r')
-    img = img.resize((256, 256))
-    input = totensor(img).unsqueeze(0)
-    output = model(input)
-    pred = int(output.argmax(dim=1))
-    conf = float(output.max(dim=1).values)
-    diff = float(output.max(dim=1).values) - float(output.min(dim=1).values)
-    print(os.path.basename(file).split('_')[0], pred, conf, diff, os.path.basename(file))
+    return data_list
 
-    # if pred != int(os.path.basename(file).split('_')[0]):
-    #     shutil.copy(file, 'data/res_error')
-    #     print(os.path.basename(file).split('_')[0], pred, conf, diff, os.path.basename(file))
 
+def file_load(path, data_type):
+    totensor = transforms.ToTensor()
+    if data_type == 'u':
+        response = requests.get(path)
+        img = Image.open(BytesIO(response.content))
+        img = img.resize((256, 256))
+        image = totensor(img).unsqueeze(0).to(device)
+    elif data_type == 'f' or data_type == 'd':
+        img = Image.open(path, 'r')
+        img = img.resize((256, 256))
+        image = totensor(img).unsqueeze(0).to(device)
+    else:
+        print('file type error!!!')
+
+    return image
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    model = torch.load(args.checkpoint, map_location=torch.device(device))
+    model.eval()
+
+    if args.data_type == 'f' or args.data_type == 'u':
+        input_im = file_load(args.data_path, args.data_type)
+        output = model(input_im)
+        pred = int(output.argmax(dim=1))
+        conf = float(output.max(dim=1).values)
+        print(f"{os.path.basename(args.data_path)} --> {'Day' if pred == 0 else 'Night'} || Confidence : {conf}")
+
+    elif args.data_type == 'd':
+        data_list = file_list_load(args.data_path)
+        for file in tqdm(data_list):
+            input_im = file_load(file, args.data_type)
+            output = model(input_im)
+            pred = int(output.argmax(dim=1))
+            conf = float(output.max(dim=1).values)
+            diff = float(output.max(dim=1).values) - float(output.min(dim=1).values)
+
+            acc = 0
+            label = -1
+
+            if len(os.path.basename(file).split('_')) > 1:
+                label = int(os.path.basename(file).split('_')[0])
+                if pred == label:
+                    acc += 1.0
+                else:
+                    if args.file_copy:
+                        shutil.copy(file, r'D:\Data\res_error' + f'\\{pred}_{conf:.6f}.jpg')
+
+                if args.print:
+                    print(label, pred, conf, diff, os.path.basename(file))
+
+            else:
+                if args.print:
+                    print(f"{os.path.basename(file)} --> {'Day' if pred == 0 else 'Night'} || Confidence : {conf}")
+            if args.file_copy:
+                if conf < 1.0:
+                    shutil.copy(file, r'D:\Data\res_rowconf'+f'\\{pred}_{conf:.6f}.jpg')
+                elif pred == 0:
+                    shutil.copy(file, r'D:\Data\res_day'+f'\\{pred}_{conf:.6f}.jpg')
+                elif pred == 1:
+                    shutil.copy(file, r'D:\Data\res_night'+f'\\{pred}_{conf:.6f}.jpg')
+
+        accuracy = acc / len(data_list)
+        print(f'- acc: {accuracy:.4f} = {acc}/{len(data_list)}\n')
+
+    else:
+        print('file type error!!!')
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 from linformer import Linformer
 from PIL import Image
 from sklearn.model_selection import train_test_split
@@ -16,58 +17,10 @@ from tqdm import tqdm
 # from vit_pytorch.efficient import ViT
 from vit_pytorch.vit_for_small_dataset import ViT
 
-# Training settings
-batch_size = 64
-epochs = 20
-lr = 3e-5
-gamma = 0.7
-seed = 42
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 
-device = 'cpu'
-os.makedirs('data', exist_ok=True)
-
-train_dir = 'data/train'
-train_list = glob.glob(os.path.join(train_dir, '*.jpg'))
-
-labels = [os.path.basename(path).split('_')[0] for path in train_list]
-
-train_list, valid_list = train_test_split(train_list,
-                                          test_size=0.2,
-                                          stratify=labels,
-                                          random_state=seed)
-
-print(f"Train Data: {len(train_list)}")
-print(f"Validation Data: {len(valid_list)}")
-
-train_transforms = transforms.Compose(
-    [
-        transforms.Resize((224, 224)),
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-    ]
-)
-
-val_transforms = transforms.Compose(
-    [
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-    ]
-)
-
-
-test_transforms = transforms.Compose(
-    [
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-    ]
-)
-
-
-class CatsDogsDataset(Dataset):
+class Dataset(Dataset):
     def __init__(self, file_list, transform=None):
         self.file_list = file_list
         self.transform = transform
@@ -81,87 +34,158 @@ class CatsDogsDataset(Dataset):
         img = Image.open(img_path)
         img_transformed = self.transform(img)
 
-        label = int(os.path.basename(img_path).split('_')[0])
+        anno = int(os.path.basename(img_path).split('_')[0])
 
-        return img_transformed, label
-
-
-train_data = CatsDogsDataset(train_list, transform=train_transforms)
-valid_data = CatsDogsDataset(valid_list, transform=test_transforms)
-
-train_loader = DataLoader(dataset = train_data, batch_size=batch_size, shuffle=True )
-valid_loader = DataLoader(dataset = valid_data, batch_size=batch_size, shuffle=True)
-
-efficient_transformer = Linformer(
-    dim=128,
-    seq_len=49+1,  # 7x7 patches + 1 cls-token
-    depth=12,
-    heads=8,
-    k=64
-)
-
-model = ViT(
-    dim=128,
-    image_size=256,
-    patch_size=16,
-    num_classes=2,
-    # transformer=efficient_transformer,
-    channels=3,
-    depth=6,
-    heads=16,
-    mlp_dim=2048,
-    dropout=0.1,
-    emb_dropout=0.1
-).to(device)
+        return img_transformed.to(device), anno
 
 
-# loss function
-criterion = nn.CrossEntropyLoss()
-# optimizer
-optimizer = optim.Adam(model.parameters(), lr=lr)
-# scheduler
-scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Day and Night Classifier')
+    parser.add_argument('--path', default=r'D:\Data\train', help='image file path dir or url')
+    parser.add_argument('--batchs', default=64, help='image file path dir or url')
+    parser.add_argument('--epochs', default=10, help='image file path dir or url')
+    parser.add_argument('--lr', default=3e-5, help='image file path dir or url')
+    parser.add_argument('--gamma', default=0.7, help='image file path dir or url')
+    parser.add_argument('--seed', default=42, help='image file path dir or url')
+    parser.add_argument('--checkpoint', default='save2_gpu.pt', help='checkpoint file')
 
-best_loss = 1000
-best_acc = 0
+    args = parser.parse_args()
+    return args
 
-for epoch in range(epochs):
-    epoch_loss = 0
-    epoch_accuracy = 0
 
-    for data, label in tqdm(train_loader):
-        data = data.to(device)
-        label = label.to(device)
+def get_dataloader(args):
+    data_list = file_list_load(args.path)
+    labels = [os.path.basename(args.path).split('_')[0] for path in data_list]
+    train_list, valid_list = train_test_split(data_list,
+                                              test_size=0.2,
+                                              stratify=labels,
+                                              random_state=args.seed)
 
-        output = model(data)
-        loss = criterion(output, label)
+    print(f"Train Data: {len(train_list)}")
+    print(f"Validation Data: {len(valid_list)}")
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    [train_transforms, test_transforms] = get_transform()
 
-        acc = (output.argmax(dim=1) == label).float().mean()
-        epoch_accuracy += acc / len(train_loader)
-        epoch_loss += loss / len(train_loader)
+    train_data = Dataset(train_list, transform=train_transforms)
+    valid_data = Dataset(valid_list, transform=test_transforms)
 
-    with torch.no_grad():
-        epoch_val_accuracy = 0
-        epoch_val_loss = 0
-        for data, label in valid_loader:
-            data = data.to(device)
-            label = label.to(device)
+    t_loader = DataLoader(dataset=train_data, batch_size=args.batchs, shuffle=True)
+    v_loader = DataLoader(dataset=valid_data, batch_size=args.batchs, shuffle=True)
 
-            val_output = model(data)
-            val_loss = criterion(val_output, label)
+    return [t_loader, v_loader]
 
-            acc = (val_output.argmax(dim=1) == label).float().mean()
-            epoch_val_accuracy += acc / len(valid_loader)
-            epoch_val_loss += val_loss / len(valid_loader)
 
-    print(
-        f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n"
+def file_list_load(path):
+    data_list = glob.glob(os.path.join(path, '*.jpg'))
+    return data_list
+
+
+def get_transform():
+    train_transforms = transforms.Compose(
+        [
+            transforms.Resize((256, 256)),
+            transforms.RandomChoice([
+                transforms.RandomResizedCrop(256),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.RandomRotation(30),
+                transforms.RandomPerspective()]),
+            transforms.ToTensor(),
+            transforms.RandomErasing(),
+        ]
     )
-    if epoch_val_loss < best_loss and epoch_val_accuracy > best_acc:
-        torch.save(model, 'save1.pt')
-        best_loss = epoch_val_loss
-        best_acc = epoch_val_accuracy
+
+    val_transforms = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.ToTensor(),
+        ]
+    )
+    return [train_transforms, val_transforms]
+
+
+def get_models(args):
+    if args.checkpoint:
+        model = torch.load(args.checkpoint, map_location=torch.device(device))
+        print(args.checkpoint + ' is loaded....')
+
+    else:
+        efficient_transformer = Linformer(
+            dim=128,
+            seq_len=49+1,  # 7x7 patches + 1 cls-token
+            depth=12,
+            heads=8,
+            k=64
+        )
+        model = ViT(
+            dim=128,
+            image_size=256,
+            patch_size=16,
+            num_classes=2,
+            # transformer=efficient_transformer,
+            channels=3,
+            depth=6,
+            heads=16,
+            mlp_dim=2048,
+            dropout=0.1,
+            emb_dropout=0.1
+        ).to(device)
+    return model
+
+
+def get_loss():
+    # loss function
+    crit = nn.CrossEntropyLoss()
+    return crit
+
+
+def get_scheduler(model, args):
+    # optimizer
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    # scheduler
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    return [optimizer, scheduler]
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    best_loss = 1000
+    best_acc = 0
+
+    [train_loader, valid_loader] = get_dataloader(args)
+    model = get_models(args)
+    criterion = get_loss()
+    [optimizer, scheduler] = get_scheduler(model, args)
+
+    for epoch in range(args.epochs):
+        epoch_loss = epoch_accuracy = 0
+        for data, label in tqdm(train_loader):
+            output = model(data)
+            loss = criterion(output, label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            acc = (output.argmax(dim=1) == label).float().mean()
+            epoch_accuracy += acc / len(train_loader)
+            epoch_loss += loss / len(train_loader)
+
+        with torch.no_grad():
+            epoch_val_accuracy = epoch_val_loss = 0
+            for data, label in valid_loader:
+                val_output = model(data)
+                val_loss = criterion(val_output, label)
+
+                acc = (val_output.argmax(dim=1) == label).float().mean()
+                epoch_val_accuracy += acc / len(valid_loader)
+                epoch_val_loss += val_loss / len(valid_loader)
+
+        print(
+            f"Epoch : {epoch+1} - loss : {epoch_loss:.4f} - acc: {epoch_accuracy:.4f} - val_loss : {epoch_val_loss:.4f} - val_acc: {epoch_val_accuracy:.4f}\n"
+        )
+        if epoch_val_loss < best_loss and epoch_val_accuracy > best_acc:
+            torch.save(model, 'save2-1.pt')
+            best_loss = epoch_val_loss
+            best_acc = epoch_val_accuracy
